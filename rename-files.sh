0b7708517a16ff0cc4b62702e73938aa2dae319c
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2020-07-25 03:02:57 +0100

    update usage info

--- rename-files.sh
+++ rename-files.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 <<DETAILS
-Rename all files in the specified directories to replace spaces with
-underscores, and convert file name to lower case.
+Rename individual files, or all files in the specified directories, to 
+replace spaces with underscores, and convert file name to lower case.
 
 Usage:
 1. Rename all files in current directory
     $ ./remove-spaces.sh ./
 2. Rename all files in multiple directories
     $ ./remove-spaces.sh dir1/ dir2/ dir3/
+3. Rename individual files
+    $ ./remove-spaces.sh file1 file2 file3
 DETAILS
 
 # Change Internal Field Separator to simplify file renaming process (for file names containing spaces) 
