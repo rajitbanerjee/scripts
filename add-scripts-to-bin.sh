728ca7da25e6a8b1d2742a4e6f8dc183d3c7e7b2
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2020-07-26 17:52:20 +0100

    add scripts to bin

---
+++ add-scripts-to-bin.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+<<DETAILS
+Copy all python/bash scripts (without their extensions) in the specified directory 
+to another specified destination.
+
+Ideally, the destination folder should be added to $PATH.
+This allows to scripts to be executed from any working directory!
+
+Usage:
+    $ ./add-scripts-to-bin.sh <dir> <destination>
+
+Default <dir> is `./`
+Default <destination> is `/usr/local/bin/custom_scripts`.
+
+E.g.
+    $ ./add-scripts-to-bin.sh
+DETAILS
+
+SRC="./"
+if [[ $1 != "" ]]; then
+    let SRC=$1
+fi
+
+DEST="/usr/local/bin/custom_scripts"
+if [[ $2 != "" ]]; then
+    let DEST=$2
+fi
+
+sudo mkdir -p $DEST
+
+for FILE in $1*; do
+    echo $FILE
+    if [[ -f $FILE ]] && ([[ ${FILE: -3} == ".py" ]] || [[ ${FILE: -3} == ".sh" ]]) ; then
+        NAME=$(basename $FILE)
+        sudo cp $FILE "$DEST/${NAME%.*}"
+        chmod +x $DEST/*
+    fi
+done
